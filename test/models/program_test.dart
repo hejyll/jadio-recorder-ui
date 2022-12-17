5ad00f287e9590a7e12c92261b3828c1ff18c51e
Hejiko Hejio <hejyll@gmail.com>
2022-12-17 22:47:35 +0900

    Fix program_test.dart

--- test/models/program_test.dart
+++ test/models/program_test.dart
@@ -5,55 +5,27 @@ import 'package:jadio_recorder_ui/models/program.dart';
 
 void main() {
   test('Test Program constructor', () {
-    final program = Program(
-      id: 1000,
-      stationId: 'DUMMY',
-      name: 'This is program name',
-      url: 'https://test.html',
-      description: 'This is description',
-      information: 'This is information',
-      performers: [
-        'Hoge',
-        'Fuga',
-      ],
-      copyright: 'copyright@test',
-      episodeId: 31415,
-      episodeName: '#100',
-      datetime: DateTime(2022, 12, 17, 10, 00),
-      duration: 3600,
-      asciiName: 'hoge',
-      guests: [
-        'Hage',
-      ],
-      imageUrl: 'https://test.jpg',
-      isVideo: false,
-      status: 'fetched',
-    );
-
-    expect(program.id, 1000);
-    expect(program.stationId, 'DUMMY');
-    expect(program.name, 'This is program name');
-    expect(program.url, 'https://test.html');
-    expect(program.description, 'This is description');
-    expect(program.information, 'This is information');
-    expect(program.performers, ['Hoge', 'Fuga']);
-    expect(program.copyright, 'copyright@test');
-    expect(program.episodeId, 31415);
-    expect(program.episodeName, '#100');
-    expect(program.datetime, DateTime(2022, 12, 17, 10, 00));
-    expect(program.duration, 3600);
-    expect(program.asciiName, 'hoge');
-    expect(program.guests, ['Hage']);
-    expect(program.imageUrl, 'https://test.jpg');
-    expect(program.isVideo, false);
-    expect(program.status, 'fetched');
-  });
-
-  test('Test Program.fromJson', () {
     final jsonData =
         json.decode(File('test/fixtures/programs.json').readAsStringSync())[0];
-    final program = Program.fromJson(jsonData);
-    jsonData["datetime"] = DateTime.parse(jsonData["datetime"]);
+    final program = Program(
+      id: jsonData['id'],
+      stationId: jsonData['stationId'],
+      name: jsonData['name'],
+      url: jsonData['url'],
+      description: jsonData['description'],
+      information: jsonData['information'],
+      performers: jsonData['performers'].cast<String>(),
+      copyright: jsonData['copyright'],
+      episodeId: jsonData['episodeId'],
+      episodeName: jsonData['episodeName'],
+      datetime: DateTime.parse(jsonData["datetime"]),
+      duration: jsonData['duration'],
+      asciiName: jsonData['asciiName'],
+      guests: jsonData['guests'].cast<String>(),
+      imageUrl: jsonData['imageUrl'],
+      isVideo: jsonData['isVideo'],
+      status: jsonData['status'],
+    );
 
     expect(program.id, jsonData['id']);
     expect(program.stationId, jsonData['stationId']);
@@ -65,7 +37,31 @@ void main() {
     expect(program.copyright, jsonData['copyright']);
     expect(program.episodeId, jsonData['episodeId']);
     expect(program.episodeName, jsonData['episodeName']);
-    expect(program.datetime, jsonData['datetime']);
+    expect(program.datetime, DateTime.parse(jsonData["datetime"]));
+    expect(program.duration, jsonData['duration']);
+    expect(program.asciiName, jsonData['asciiName']);
+    expect(program.guests, jsonData['guests']);
+    expect(program.imageUrl, jsonData['imageUrl']);
+    expect(program.isVideo, jsonData['isVideo']);
+    expect(program.status, jsonData['status']);
+  });
+
+  test('Test Program.fromJson', () {
+    final jsonData =
+        json.decode(File('test/fixtures/programs.json').readAsStringSync())[0];
+    final program = Program.fromJson(jsonData);
+
+    expect(program.id, jsonData['id']);
+    expect(program.stationId, jsonData['stationId']);
+    expect(program.name, jsonData['name']);
+    expect(program.url, jsonData['url']);
+    expect(program.description, jsonData['description']);
+    expect(program.information, jsonData['information']);
+    expect(program.performers, jsonData['performers']);
+    expect(program.copyright, jsonData['copyright']);
+    expect(program.episodeId, jsonData['episodeId']);
+    expect(program.episodeName, jsonData['episodeName']);
+    expect(program.datetime, DateTime.parse(jsonData["datetime"]));
     expect(program.duration, jsonData['duration']);
     expect(program.asciiName, jsonData['asciiName']);
     expect(program.guests, jsonData['guests']);
